0873d7cad43e8b05370f7630f33e97cd9d140a44
Dhruv1001 <buddhabhattidhruv@gmail.com>
2025-11-09 13:17:13 +0530

    smart contract

--- TokenMarketPlace.sol
+++ TokenMarketPlace.sol
@@ -1,10 +1,11 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+pragma solidity ^0.8.0;
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "hardhat/console.sol";//to console output
 
-contract TokenMarketPlace is Ownable(msg.sender){
+contract TokenMarketPlace is Ownable{
     
 using SafeERC20 for IERC20;
 using SafeMath for uint256;   //x.add(y) = x+y
@@ -21,5 +22,49 @@ event TokenPriceUpdated(uint256 newPrice);
 event TokenBought(address indexed buyer, uint256 amount, uint256 totalCost);
 event TokenSold(address indexed seller, uint256 amount, uint256 totalEarned);
 event TokensWithdrawn(address indexed owner, uint256 amount);
+event EtherWithdrawn(address indexed owner, uint256 amount);
+event CalculateTokenPrice(uint256 priceToPay);
+
+constructor(address _gldToken) Ownable(msg.sender){
+    gldToken = IERC20(_gldToken);
+}
+
+function adjustTokenpriceBaseOnDemand() public {
+    uint marketDemandRation = buyerCount.mul(1e18).div(sellerCount);
+    uint smoothingFactor = 1e18;
+    uint adjustedRatio = marketDemandRation.add(smoothingFactor).div(2);
+    if(prevAdjustedRatio!=adjustedRatio){
+        prevAdjustedRatio=adjustedRatio;
+        uint newTokenPrice = tokenPrice.mul(adjustedRatio).div(1e18);
+        uint minimumPrice = 2e16;
+        if(newTokenPrice<minimumPrice){
+            tokenPrice = minimumPrice;
+        }
+        tokenPrice = newTokenPrice;
+
+    }
+
+
+}
+
+function buyGLDToken(uint256 _amoutOfToken) public payable {
+
+}
+
+function calculateTokenPrice(uint _amoutOfToken) public returns(uint){
+
+}
+
+function sellGLDToken(uint256 _amountOftoken) public {
+
+}
+
+function withdrawTokens(uint256 _amount) public onlyOwner{
+
+}
+
+function withdrawEther(uint256 _amount) public onlyOwner{
+
+}
 
 }
